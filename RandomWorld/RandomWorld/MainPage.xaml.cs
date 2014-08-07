e87afd3bde73ab1de13a3455ecf25b6aeeb96665
Juan Carlos <juniocarl@gmail.com>
2014-08-07 00:34:42 -0500

    V 1.0 Numeros y letras.

--- RandomWorld/RandomWorld/MainPage.xaml.cs
+++ RandomWorld/RandomWorld/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
@@ -25,8 +26,8 @@ namespace RandomWorld
         public MainPage()
         {
             this.InitializeComponent();
-
             this.NavigationCacheMode = NavigationCacheMode.Required;
+            RadioNumber.IsChecked = true;
         }
 
         /// <summary>
@@ -43,6 +44,97 @@ namespace RandomWorld
             // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
+        }
+
+        private string prevInput;
+
+        private void RadioNumber_Checked(object sender, RoutedEventArgs e)
+        {
+            TextMinimum.Text = "1";
+            TextMaximum.Text = "10";
+
+            TextMinimum.MaxLength = 9;
+            TextMaximum.MaxLength = 9;
+        }
+
+        private void RadioLetter_Checked(object sender, RoutedEventArgs e)
+        {
+            TextMinimum.Text = "A";
+            TextMaximum.Text = "Z";
+
+            TextMinimum.MaxLength = 1;
+            TextMaximum.MaxLength = 1;
+        }
+
+        private void Text_GotFocus(object sender, RoutedEventArgs e)
+        {
+            prevInput = (sender as TextBox).Text;
+        }
+
+        private void Text_LostFocus(object sender, RoutedEventArgs e)
+        {
+            string value = (sender as TextBox).Text;
+
+            if(RadioNumber.IsChecked == true)
+            {
+                // Si es numero, validar que sea número
+                try { Int32.Parse(value);  }
+                catch(Exception)
+                {
+                    ShowMessage("El valor ingresado no es numérico.");
+                    (sender as TextBox).Text = prevInput;
+                    return;
+                }
+
+                if (Int32.Parse(TextMinimum.Text) > Int32.Parse(TextMaximum.Text))
+                {
+                    ShowMessage("El valor mínimo debe ser menor o igual al valor máximo.");
+                    (sender as TextBox).Text = prevInput;
+                    return;
+                }
+            }
+            else
+            {
+                if(value.ToUpper()[0] < 'A' || value.ToUpper()[0] > 'Z')
+                {
+                    ShowMessage("El valor ingresado no es una letra.");
+                    (sender as TextBox).Text = prevInput;
+                    return;
+                }
+                if (TextMinimum.Text.ToUpper()[0] > TextMaximum.Text.ToUpper()[0])
+                {
+                    ShowMessage("El valor mínimo debe ser menor o igual al valor máximo.");
+                    (sender as TextBox).Text = prevInput;
+                    return;
+                }
+            }
+
+        }
+
+        private void ShowMessage(string str)
+        {
+            MessageDialog ms = new MessageDialog(str);
+            ms.ShowAsync();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Random random = new System.Random();
+            string result = null;
+            
+            if(RadioNumber.IsChecked == true)
+            {
+                result = (random.Next(Int32.Parse(TextMinimum.Text), Int32.Parse(TextMaximum.Text))).ToString();
+            }
+            else
+            {
+                int min = TextMinimum.Text.ToUpper()[0] - 'A';
+                int max = TextMaximum.Text.ToUpper()[0] - 'A';
+                char c = ((char)(random.Next(min, max) + 'A'));
+                result = c.ToString();
+            }
+
+            DisplayedValue.Text = result;
         }
     }
 }
